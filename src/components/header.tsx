2b108be809d5d04dd02eb1d6afeedc9ca0fcf307
Zain Gulbaz <zain.gulbaz@vetdrive.co>
2024-02-25 00:31:13 +0500

    Changed the font in header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,10 +7,10 @@ const Header =async () => {
     const emailAddress=user?.emailAddresses;
     
   return (
-    <div className="flex justify-between p-8 pr-16 gap-2 bg-gray-100 shadow-xl text-xl font-bold items-center">
+    <div className="flex justify-between p-4 px-8 gap-2 bg-gray-100 shadow-xl font-bold text-sm text-md-lg items-center">
        Next and Clerk
       {(emailAddress)?<div className="flex gap-2 items-center "><Link href={"/dashboard"}>Dashboard</Link>
-      <Link href={"/"} className="mr-4">Home</Link> <UserButton/> </div>:<div className="flex gap-2 items-center"><Link href={"/sign-up"}>Sign Up</Link>
+      <Link href={"/"} className="mr-4">Home</Link> <UserButton/> </div>:<div className="flex gap-2 items-center"><Link href={"/sign-up"}>Sign up</Link>
       <Link href={"/sign-in"}>Sign in</Link></div>}
     </div>
   )
